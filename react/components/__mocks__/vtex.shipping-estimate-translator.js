c0bd50ad10ff9c29ae6253c2de7702feb6a78140
nandoacoelho <nandoacoelho@gmail.com>
2019-03-11 17:39:18 -0300

    Remove unused locales

--- react/components/__mocks__/vtex.shipping-estimate-translator.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class TranslateEstimate extends Component {
-  render() {
-    return <div>TranslateEstimate</div>
-  }
-}
-
-export default TranslateEstimate
