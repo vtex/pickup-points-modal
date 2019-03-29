331b8e106768aeb2b3fdd7e8728337f3fe3b26d6
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2019-03-29 16:10:01 -0300

    Add CSS modules to Error component

--- react/components/Error.js
+++ react/components/Error.js
@@ -6,7 +6,7 @@ import PinBlocked from '../assets/components/PinBlocked'
 import PinLocationUnknown from '../assets/components/PinLocationUnknown'
 import PinNoPickups from '../assets/components/PinNoPickups'
 
-import './Error.css'
+import styles from './Error.css'
 
 import GeolocationStatus from './GeolocationStatus'
 import Button from './Button'
@@ -34,7 +34,7 @@ export class Error extends Component {
     const { intl, status, onManualGeolocationError } = this.props
     const subtitleString = this.getSubtitleString()
     return (
-      <div className="pkpmodal-locating-wrapper-error">
+      <div className={`pkpmodal-locating-wrapper-error ${styles.wrapper}`}>
         <GeolocationStatus
           Image={() => (
             <div>
@@ -49,7 +49,7 @@ export class Error extends Component {
           )}
           subtitleBottom={subtitleString}
           titleBottom={status}>
-          <div className="pkpmodal-locating-error-manual">
+          <div className={`pkpmodal-locating-error-manual ${styles.manual}`}>
             <Button
               kind="primary"
               large
