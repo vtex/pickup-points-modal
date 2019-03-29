843c16f9da6cde3649aa96e10075cab503b3da84
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2019-03-29 16:10:22 -0300

    Fix CSS modules in CloseButton component

--- react/components/CloseButton.js
+++ react/components/CloseButton.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-import './CloseButton.css'
+import styles from './CloseButton.css'
 
 export class CloseButton extends PureComponent {
   render() {
