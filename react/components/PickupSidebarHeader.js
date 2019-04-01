f2ccc460d468ff1f1312a77d925b48cc2e05a219
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2019-04-01 14:49:05 -0300

    Add CSS modules to PickupSidebarHeader component

--- react/components/PickupSidebarHeader.js
+++ react/components/PickupSidebarHeader.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
 import { translate } from '../utils/i18nUtils'
 
+import styles from './PickupSidebarHeader.css'
+
 export class PickupSidebarHeader extends PureComponent {
   render() {
     const { intl, isPickupDetailsActive } = this.props
 
     return (
-      <div className="pkpmodal-header">
-        <h4 className="pkpmodal-title">
+      <div className={`pkpmodal-header ${styles.header}`}>
+        <h4 className={`pkpmodal-title ${styles.title}`}>
           {isPickupDetailsActive
             ? translate(intl, 'pointDetails')
             : translate(intl, 'selectPickupPoint')}
