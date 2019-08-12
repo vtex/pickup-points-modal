188128922d1422c25714e012e5d0821238a168fc
nandoacoelho <nandoacoelho@gmail.com>
2019-08-12 00:16:05 -0300

    Update searching message to show in sidebar

--- react/components/SearchingState.js
+++ react/components/SearchingState.js
@@ -9,7 +9,7 @@ import { GEOLOCATION_SEARCHING, SEARCHING } from '../constants'
 
 class SearchingState extends PureComponent {
   render() {
-    const { intl, isFullPage, activeState } = this.props
+    const { activeSidebarState, activeState, intl, isFullPage } = this.props
 
     return (
       <div
@@ -22,7 +22,7 @@ class SearchingState extends PureComponent {
           } pkpmodal-search-alone`}>
           <Spinner size={24} />
           <p className={searchingStyles.searching}>
-            {activeState === SEARCHING &&
+            {(activeState === SEARCHING || activeSidebarState === SEARCHING) &&
               translate(intl, 'geolocationSearching')}
             {activeState === GEOLOCATION_SEARCHING &&
               translate(intl, 'geolocationWaiting')}
@@ -40,6 +40,7 @@ SearchingState.defaultProps = {
 SearchingState.propTypes = {
   isFullPage: PropTypes.bool.isRequired,
   activeState: PropTypes.string,
+  activeSidebarState: PropTypes.string,
   intl: intlShape.isRequired,
 }
 
