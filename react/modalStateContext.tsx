8ffc5f804ae3160c5e21dacfe216e027a558d01d
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-11-17 18:06:09 -0300

    Add types for modal state context

--- react/modalStateContext.tsx
+++ react/modalStateContext.tsx
@@ -1,80 +1,81 @@
-import React from 'react'
+import type { ComponentType } from 'react'
+import React, { useContext } from 'react'
 
-export const ModalStateContext = React.createContext()
+import type {
+  ActiveState,
+  SidebarActiveState,
+  GeolocationStatus,
+} from './utils/StateUtils'
 
-export function injectState(Component) {
-  return function StateInjectedComponent(props) {
-    return (
-      <ModalStateContext.Consumer>
-        {({
-          activeState,
-          activeSidebarState,
-          bestPickupOptions,
-          externalPickupPoints,
-          geolocationStatus,
-          hoverPickupPoint,
-          isSearching,
-          isSelectedBestPickupPoint,
-          searchPickupsInArea,
-          lastState,
-          lastSidebarState,
-          lastMapCenterLatLng,
-          logisticsInfo,
-          pickupOptions,
-          pickupPoints,
-          residentialAddress,
-          searchedAreaNoPickups,
-          selectNextPickupPoint,
-          selectPreviousPickupPoint,
-          selectedPickupPoint,
-          setGeolocationStatus,
-          setHoverPickupPoint,
-          setActiveState,
-          setActiveSidebarState,
-          setAskForGeolocation,
-          setSelectedPickupPoint,
-          setMapCenterLatLng,
-          setShouldSearchArea,
-          setShowOtherPickupPoints,
-          shouldSearchArea,
-          showOtherPickupPoints,
-        }) => (
-          <Component
-            {...props}
-            activeState={activeState}
-            activeSidebarState={activeSidebarState}
-            bestPickupOptions={bestPickupOptions}
-            externalPickupPoints={externalPickupPoints}
-            geolocationStatus={geolocationStatus}
-            hoverPickupPoint={hoverPickupPoint}
-            isSearching={isSearching}
-            isSelectedBestPickupPoint={isSelectedBestPickupPoint}
-            lastState={lastState}
-            lastSidebarState={lastSidebarState}
-            lastMapCenterLatLng={lastMapCenterLatLng}
-            logisticsInfo={logisticsInfo}
-            selectedPickupPoint={selectedPickupPoint}
-            pickupOptions={pickupOptions}
-            pickupPoints={pickupPoints}
-            residentialAddress={residentialAddress}
-            searchedAreaNoPickups={searchedAreaNoPickups}
-            searchPickupsInArea={searchPickupsInArea}
-            selectNextPickupPoint={selectNextPickupPoint}
-            selectPreviousPickupPoint={selectPreviousPickupPoint}
-            setGeolocationStatus={setGeolocationStatus}
-            setHoverPickupPoint={setHoverPickupPoint}
-            setActiveState={setActiveState}
-            setActiveSidebarState={setActiveSidebarState}
-            setAskForGeolocation={setAskForGeolocation}
-            setMapCenterLatLng={setMapCenterLatLng}
-            setSelectedPickupPoint={setSelectedPickupPoint}
-            setShouldSearchArea={setShouldSearchArea}
-            setShowOtherPickupPoints={setShowOtherPickupPoints}
-            shouldSearchArea={shouldSearchArea}
-            showOtherPickupPoints={showOtherPickupPoints}
-          />
-        )}
-      </ModalStateContext.Consumer>
-    )
+// TODO: we should replace the remaining `any` types with the actual types for
+// the properties
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface ModalState {
+  activeState: ActiveState
+  activeSidebarState: SidebarActiveState
+  bestPickupOptions: any[]
+  externalPickupPoints: any[]
+  geolocationStatus: GeolocationStatus
+  hoverPickupPoint: any
+  isSearching: boolean
+  isSelectedBestPickupPoint: boolean
+  searchPickupsInArea: (geoCoordinates: any, address: any) => void
+  lastState: ActiveState
+  lastSidebarState: SidebarActiveState
+  lastMapCenterLatLng: google.maps.LatLng | null
+  logisticsInfo: any[] | undefined
+  pickupOptions: any[] | undefined
+  pickupPoints: any[] | undefined
+  residentialAddress: any
+  searchedAreaNoPickups: boolean
+  selectNextPickupPoint: () => void
+  selectPreviousPickupPoint: () => void
+  selectedPickupPoint: any
+  setGeolocationStatus: GeolocationStatus
+  setHoverPickupPoint: (pickupPoint: any) => void
+  setActiveState: (state: ActiveState) => void
+  setActiveSidebarState: (sidebarState: SidebarActiveState) => void
+  setAskForGeolocation: (askForGeolocation: boolean) => void
+  setSelectedPickupPoint: (value: {
+    pickupPoint: any
+    isBestPickupPoint: boolean
+  }) => void
+  setMapCenterLatLng: (center: google.maps.LatLng) => void
+  setShouldSearchArea: (shouldSearch: boolean) => void
+  setShowOtherPickupPoints: (showOtherPickupPoints: boolean) => void
+  shouldSearchArea: boolean
+  showOtherPickupPoints: boolean
+}
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+export const ModalStateContext = React.createContext<ModalState | undefined>(
+  undefined
+)
+
+export const useModalState = () => {
+  const context = useContext(ModalStateContext)
+
+  if (context === undefined) {
+    throw new Error('useModalState must be used inside <ModalState />')
   }
+
+  return context
+}
+
+export function injectState<T>(Component: ComponentType<T & ModalState>) {
+  const StateInjectedComponent: React.FC<T> = (props) => {
+    let context
+
+    try {
+      context = useModalState()
+    } catch (e) {
+      throw new Error(`injectState(${Component.displayName}): ${e.message}`)
+    }
+
+    return <Component {...props} {...context} />
+  }
+
+  StateInjectedComponent.displayName = `withModalState(${Component.displayName})`
+
+  return StateInjectedComponent
 }
--- react/utils/StateUtils.ts
+++ react/utils/StateUtils.ts
@@ -6,22 +6,34 @@ import {
   GEOLOCATION_SEARCHING,
 } from '../constants'
 
-export function isDifferentGeoCoords(a, b) {
+export type GeolocationStatus =
+  | 'prompt'
+  | 'searching'
+  | 'errorNotAllowed'
+  | 'errorNotFound'
+  | 'errorCouldNotGetLocation'
+
+export function isDifferentGeoCoords(a: [number, number], b: [number, number]) {
   return a[0] !== b[0] || a[1] !== b[1]
 }
 
-export function isCurrentState(state, activeState) {
+export function isCurrentState(state: ActiveState, activeState: ActiveState) {
   return state === activeState
 }
 
-export function isCurrentStateFromAllStates(currentState, states) {
+export function isCurrentStateFromAllStates(
+  currentState: ActiveState | SidebarActiveState,
+  states: { activeState: ActiveState; activeSidebarState: SidebarActiveState }
+) {
   return (
     currentState === states.activeState ||
     currentState === states.activeSidebarState
   )
 }
 
-export const getInitialActiveState = props => {
+export type ActiveState = 'sidebar' | 'geolocation_searching' | 'initial'
+
+export const getInitialActiveState = (props: any): ActiveState => {
   if (props.askForGeolocation) {
     return GEOLOCATION_SEARCHING
   }
@@ -33,7 +45,11 @@ export const getInitialActiveState = props => {
   return INITIAL
 }
 
-export const getInitialActiveSidebarState = props => {
+export type SidebarActiveState = 'geolocation_searching' | 'details' | 'list'
+
+export const getInitialActiveSidebarState = (
+  props: any
+): SidebarActiveState => {
   if (props.askForGeolocation) {
     return GEOLOCATION_SEARCHING
   }
@@ -45,12 +61,9 @@ export const getInitialActiveSidebarState = props => {
   return LIST
 }
 
-export function getCleanId(id) {
-  return (
-    id &&
-    id
-      .replace(/[^\w\s]/gi, '')
-      .split(' ')
-      .join('-')
-  )
+export function getCleanId(id?: string) {
+  return id
+    ?.replace(/[^\w\s]/gi, '')
+    .split(' ')
+    .join('-')
 }
