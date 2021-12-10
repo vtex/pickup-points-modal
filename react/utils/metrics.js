df4cad4b11de5f1d3b95786821270a514e2ec105
lbebber <lucasbbebber@gmail.com>
2021-12-10 11:40:32 -0300

    Create usage metrics

--- react/utils/metrics.js
+++ react/utils/metrics.js
@@ -14,6 +14,34 @@ export function searchPickupAddressByGeolocationEvent(data) {
   })
 }
 
+export function openPickupPointsModalEvent() {
+  log({ name: 'openPickupPointsModal' })
+}
+
+export function closePickupPointsModalEvent() {
+  log({ name: 'closePickupPointsModal' })
+}
+
+export function pickupPointConfirmationEvent() {
+  log({ name: 'pickupPointConfirmation' })
+}
+
+//// the strings below help keeping track of how
+//// the user has selected the pickup point
+// selected pickup point from the map
+export const SELECTION_METHOD_MAP = 'map'
+// selected pickup point from the list
+export const SELECTION_METHOD_LIST = 'list'
+// selected pickup point from the list, after clicking "view all pickup points"
+export const SELECTION_METHOD_LIST_OTHERS = 'list-others'
+
+export function pickupPointSelectionEvent({ selectionMethod }) {
+  log({
+    name: 'pickupPointSelection',
+    data: { selectionMethod }
+  })
+}
+
 function log(data) {
   /* istanbul ignore else */
   if (process.env.NODE_ENV !== 'development' && window.checkoutLogger) {
