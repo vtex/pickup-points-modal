491789cc2daaa01224fb476b0c1a213297372392
Diego Leone <diego.leone@vtex.com.br>
2022-07-23 11:42:33 +0200

    feature/call-external-modal

--- react/index.js
+++ react/index.js
@@ -1,3 +1,4 @@
-import PickupPointsModal from './PickupPointsModal'
+//import PickupPointsModal from './PickupPointsModal'
+import PickupPointsModal  from 'arcaplanet.pickup-points-modal/react/PickupPointsModal'
 
 export default { PickupPointsModal }
